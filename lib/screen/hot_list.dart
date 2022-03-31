8655cdd19c92108fcc5c8bbb3cce554095d3007e
minjilee-03 <mj0524321@naver.com>
2022-03-31 14:21:42 +0900

    feat : create hot_list page, library page

---
+++ lib/screen/hot_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HotList extends StatelessWidget {
+  const HotList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('hot list'),
+    );
+  }
+}
