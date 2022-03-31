d5da10deac4408f4c7e0c148c74252784cb92475
minjilee-03 <mj0524321@naver.com>
2022-03-31 12:14:17 +0900

    refactor: refactor main structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:make_music_app/screen/home.dart';
+import 'package:make_music_app/widget/bottom.dart';
+import 'package:make_music_app/screen/hot_list.dart';
+import 'package:make_music_app/screen/library.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomePage(),
+              HotList(),
+              Library(),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigation(),
+        ),
+      ),
     );
   }
 }
